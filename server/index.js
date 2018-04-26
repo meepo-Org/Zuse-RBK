76cf6d0d32d07b1a4b8989aa329e59c7fe87b2d8
HananNouman <hnouman@hotmail.co.uk>
2018-04-26 13:53:10 +0300

    heroku

--- server/index.js
+++ server/index.js
@@ -6,9 +6,11 @@ var app = express();
  var handler=require('./request-handler.js');
  var mongoose=require('mongoose');
 
+
+app.use(express.static(__dirname + '/../react-client/dist'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/../react-client/dist'));
+
 
 app.post('/Signup', handler.signupUser);
 app.post('/Profile', handler.Profilesave);
