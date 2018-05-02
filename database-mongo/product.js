ae4c9179b9284da6da95cdd4a1c3a7ccb04138b4
abdulhameed89 <36267026+abdulhameed89@users.noreply.github.com>
2018-05-03 02:25:08 +0300

    Revert "fix conflict"

--- database-mongo/product.js
+++ database-mongo/product.js
@@ -3,7 +3,6 @@ var db = require('./config');
 //Second hand shop stuff posts.
 var products = mongoose.Schema({
 	name: { type : String, required : true },
-	userType: { type : String, required : true },
 	productName: { type : String, required : true },
 	productDisc:{ type : String, required : true },
 	productImg:{ type : String }
